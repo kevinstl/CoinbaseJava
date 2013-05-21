107e89ff747e4c6ae79b2ba906ffb94ce5f73c3a
kevinstl <kjwilde@hotmail.com>
2013-05-20 21:43:59 -0500

    Completed get balance scenario.

--- src/test/cucumber/BuyCoinSteps.java
+++ src/test/cucumber/BuyCoinSteps.java
@@ -4,6 +4,13 @@ import com.coinbase.java.client.CoinbaseClient;
 import com.coinbase.java.client.CoinbaseClientImpl;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.assertNotNull;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 
 public class BuyCoinSteps {
@@ -11,15 +18,22 @@ public class BuyCoinSteps {
   @Autowired
   private CoinbaseClient coinbaseClient; 
   
-//  public BuyCoinSteps(CoinbaseClient coinbaseClient) {
-//    this.coinbaseClient = coinbaseClient;
-//  }
+  private String balanceResponse;
   
-  //private CoinbaseClient coinbaseClient = new CoinbaseClientImpl();
+
+  @Given("^I have an instance of CoinbaseClient$")
+  public void I_have_an_instance_of_CoinbaseClient() throws Throwable {
+      assertNotNull(coinbaseClient);
+  }
   
-  @Given("^I get my balance of bitcoins$")
+  @When("^I get my balance of bitcoins$")
   public void I_get_my_balance_of_bitcoins() throws Throwable {
-    coinbaseClient.getBalance();
+    balanceResponse = coinbaseClient.getBalance();
   }
 
+  @Then("^I see that a balance is returned$")
+  public void I_see_that_a_balance_is_returned() throws Throwable {
+    assertThat(balanceResponse, containsString("amount"));
+  }
+  
 }
--- src/test/cucumber/buyCoin.feature
+++ src/test/cucumber/buyCoin.feature
@@ -3,4 +3,6 @@ Feature: buyCoin - Buy a bitcoin.
 
 Scenario: I am able to get my balance of bitcoins
 
-Given I get my balance of bitcoins
+Given I have an instance of CoinbaseClient
+When I get my balance of bitcoins
+Then I see that a balance is returned
