29c2d697963a7a109da3f8e2966d352e1e75e51b
kevinstl <kjwilde@hotmail.com>
2013-05-23 22:34:45 -0500

    Added spring config file for cukes.

---
+++ src/test/unit/com/coinbase/java/client/spring/SpringConfigTests.java
@@ -0,0 +1,21 @@
+package com.coinbase.java.client.spring;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.coinbase.java.client.CoinbaseClient;
+import com.coinbase.java.client.CoinbaseClientImpl;
+
+@Configuration
+public class SpringConfigTests {
+  
+  private @Value("${apiKey}") String apiKey;
+  
+  @Bean
+  public CoinbaseClient coinbaseClient(){
+    CoinbaseClient coinbaseClient = new CoinbaseClientImpl(apiKey);
+    return coinbaseClient;
+  }
+
+}
