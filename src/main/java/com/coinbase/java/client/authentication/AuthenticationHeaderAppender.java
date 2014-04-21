9b42942ae265dae2ec6a0115d86dbc7441580e9f
Kevin Wilde <kjwilde@hotmail.com>
2014-04-20 20:57:57 -0500

    Adding to private key authentication.

--- src/main/java/com/coinbase/java/client/authentication/AuthenticationHeaderAppender.java
+++ src/main/java/com/coinbase/java/client/authentication/AuthenticationHeaderAppender.java
@@ -1,5 +1,12 @@
 package com.coinbase.java.client.authentication;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Hex;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +17,19 @@ public class AuthenticationHeaderAppender  {
   public static final String ACCESS_SIGNATURE = "ACCESS_SIGNATURE";
   public static final String ACCESS_NONCE = "ACCESS_NONCE";
 
-  public void append(HttpUriRequest httpUriRequest) {
+  public void append(HttpUriRequest httpUriRequest, String url, String body) throws NoSuchAlgorithmException, InvalidKeyException {
     httpUriRequest.setHeader(ACCESS_KEY, "test_access_key");
-    httpUriRequest.setHeader(ACCESS_SIGNATURE, "test_access_signature");
-    httpUriRequest.setHeader(ACCESS_NONCE, "test_access_nonce");
+    
+    String nonce = "" + System.currentTimeMillis();
+	httpUriRequest.setHeader(ACCESS_NONCE, nonce);
+	
+	String message = nonce + url + (body != null ? body : null);
+	
+    Mac mac = Mac.getInstance("HmacSHA256");
+    mac.init(new SecretKeySpec(ACCESS_KEY.getBytes(), "HmacSHA256"));
+    String signature = new String(Hex.encodeHex(mac.doFinal(message.getBytes())));
+    
+    httpUriRequest.setHeader(ACCESS_SIGNATURE, signature);
   }
 
 }
--- src/test/unit/com/coinbase/java/client/authentication/AuthenticationHeaderAppenderTest.java
+++ src/test/unit/com/coinbase/java/client/authentication/AuthenticationHeaderAppenderTest.java
@@ -2,11 +2,15 @@ package com.coinbase.java.client.authentication;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -37,9 +41,9 @@ public class AuthenticationHeaderAppenderTest {
   }
   
   @Test
-  public void append_appendsRequiredAuthenticationHeaders() throws ClientProtocolException, IOException {
+  public void append_appendsRequiredAuthenticationHeaders() throws ClientProtocolException, IOException, InvalidKeyException, NoSuchAlgorithmException {
     
-    testObject.append(httpUriRequest);
+    testObject.append(httpUriRequest, "", "");
     
     assertNotNull(httpUriRequest.getHeaders(AuthenticationHeaderAppender.ACCESS_KEY)[0]);
     assertNotNull(httpUriRequest.getHeaders(AuthenticationHeaderAppender.ACCESS_SIGNATURE)[0]);
@@ -47,21 +51,41 @@ public class AuthenticationHeaderAppenderTest {
   }
   
   @Test
-  public void append_appendsNonceGreaterThanPrevious() throws ClientProtocolException, IOException {
+  public void append_appendsNonceGreaterThanPrevious() throws ClientProtocolException, IOException, InterruptedException, InvalidKeyException, NoSuchAlgorithmException {
     
-    testObject.append(httpUriRequest);
+    testObject.append(httpUriRequest, "", "");
     
-    Header headerNonce1 = httpUriRequest.getHeaders(AuthenticationHeaderAppender.ACCESS_SIGNATURE)[0];
+    Header headerNonce1 = httpUriRequest.getHeaders(AuthenticationHeaderAppender.ACCESS_NONCE)[0];
     assertNotNull(headerNonce1);
+    assertNotNull(headerNonce1.getValue());
+    Long nonce1 = new Long(headerNonce1.getValue());
+    assertNotNull(nonce1);
     
-    testObject.append(httpUriRequest);
+    Thread.sleep(1);
     
-    Header headerNonce2 = httpUriRequest.getHeaders(AuthenticationHeaderAppender.ACCESS_SIGNATURE)[0];
+    testObject.append(httpUriRequest, "", "");
+    
+    Header headerNonce2 = httpUriRequest.getHeaders(AuthenticationHeaderAppender.ACCESS_NONCE)[0];
     assertNotNull(headerNonce2);
+    assertNotNull(headerNonce2.getValue());
+    Long nonce2 = new Long(headerNonce2.getValue());
+    assertNotNull(nonce2);
+    
+    assertTrue("Nonce from second append is greater than first.", nonce1 < nonce2);
     
   }
   
-
+  @Test
+  public void append_appendsSignature() throws InvalidKeyException, NoSuchAlgorithmException{
+	  
+	    testObject.append(httpUriRequest, "", "");
+	    
+	    Header headerSignature = httpUriRequest.getHeaders(AuthenticationHeaderAppender.ACCESS_SIGNATURE)[0];
+	    assertNotNull(headerSignature);
+	    String signature = headerSignature.getValue();
+	    assertNotNull(signature);
+	    assertTrue("Signature length greater than zero.", StringUtils.length(signature) > 0);
+  }
   
 
 }
